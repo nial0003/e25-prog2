0b52b705541735e6418cc2f640bde4904beca41f
osman-butt <butt.osman@gmail.com>
2025-09-17 01:15:14 +0200

    updated examples

--- 02-javascript/03-js-dom2/examples/05-event-bubbling/app.js
+++ 02-javascript/03-js-dom2/examples/05-event-bubbling/app.js
@@ -1,19 +1,8 @@
-// Event bubbling example in JavaScript
+// TODO: demo event bubbling
+// TODO: demo event.target vs event.currentTarget
+// TODO: demo event capturing
+// TODO: demo stopPropagation()
 
-// Function to log the event target and current target
-function logEvent(event) {
-    console.log("Triggered by event on " + event.target.id)
-    console.log("Triggered by event on current target " + event.currentTarget.id)
-}
-
-// Note: The stopPropagation() method is used here to prevent the event from bubbling up further.
-
-
-// Setting only one event listener on the outermost element to
-// get the specific target that was clicked
-// document.querySelector('#outer').addEventListener('click', logEvent,true);
-// document.querySelector('#middle').addEventListener('click', logEvent,true);
-// document.querySelector('#inner').addEventListener('click', logEvent,true);
-document.querySelector('#outer').addEventListener('click', (e) => {console.log("Outer clicked");});
-document.querySelector('#middle').addEventListener('click', (e) => {console.log("Middle clicked");});
-document.querySelector('#inner').addEventListener('click', (e) => {console.log("Inner clicked");});
+const inner = document.querySelector('#inner');
+const middle = document.querySelector('#middle');
+const outer = document.querySelector('#outer');
--- 02-javascript/03-js-dom2/examples/07-game/app.js
+++ 02-javascript/03-js-dom2/examples/07-game/app.js
@@ -1,36 +1,8 @@
 // State variables
 let noOfCliks = 0;
 let maxCps = 0;
-const clickTimestamps = [];
 
-// Rolling window in milliseconds (e.g., 1000 ms = 1 second)
-const ROLLING_WINDOW = 1000;
-
-// TODO
-document.addEventListener("DOMContentLoaded", initGame);
-
-function initGame() {
-    const maxCpsLocalStorage = localStorage.getItem("maxCps");
-    if (maxCpsLocalStorage) {
-        maxClicksPerSecond = maxCpsLocalStorage;
-    } else {
-        localStorage.setItem("maxCps",maxClicksPerSecond)
-    }
-    document.querySelector("#maxClicksPerSecond").textContent = maxClicksPerSecond;
-    document.querySelector("#clickMe").addEventListener("click",handleClick);
-}
-
-function handleClick() {
-    document.querySelector("#totalClicks").textContent = ++noOfCliks;
-    const now = Date.now();
-    clickTimestamps.push(now);
-    while(now - clickTimestamps[0] > ROLLING_WINDOW) {
-        clickTimestamps.shift();
-    }
-    document.querySelector("#clicksPerSecond").textContent = clickTimestamps.length;
-    if (clickTimestamps.length) {
-        maxClicksPerSecond = maxClicksPerSecond<clickTimestamps.length ? clickTimestamps.length : maxClicksPerSecond;
-        window.localStorage.setItem("maxCps",maxClicksPerSecond);
-    }
-    document.querySelector("#maxClicksPerSecond").textContent = maxClicksPerSecond;
-}
+// TODO: Track the number of clicks
+// TODO: Track clicks pr second (cps) - use a rolling window
+// TODO: Track max cps
+// TODO: (Extra) Persist max cps in localStorage
